d72077270023d2fe7fae44a4e22d64a204576ac9
Pedro Fagundes <pedrohfagundes2002@gmail.com>
2023-10-10 01:34:23 -0300

    FundamentosGolang

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 import "fmt"
+
+//structs
+type User struct {
+	name string
+	age uint
+}
 func main(){
 	//variaveis
-	var numero int64
+	// var numero int64
 	var username string = "Pedro"
+
+	//arrays e slice
+	var arr []int //slice tamanho dinamica
+	var arr2 = []int{1,2,3,4,5,6} //array tamanho estatico
+	arr2 = append(arr2, 20)
+	fmt.Println(arr)
+	fmt.Println(arr2 ,cap(arr2))
+
+	//maps
+	obj := map[string]string{
+		"name": "Sammy",
+		"animal": "shark",
+		"color": "blue",
+		"location": "ocean",
+	}
+
+	animal := obj["animal"]
+
+
 	
-	fmt.Println(username)
-	fmt.Println(numero)
+	fmt.Println(obj["name"])
+	fmt.Println(animal)
 	funcaoNome(username)
+
+
+
+	user := User{name: "Pedro", age: 21}
+	fmt.Println(user)
 }
 
 //funcao
@@ -16,7 +46,9 @@ func funcaoNome(nome string) string{
 	return nome
 }
 
-//arrays
+
+
+
 
 
 
